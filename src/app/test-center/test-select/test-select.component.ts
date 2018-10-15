930dae31dfb2f36cf1f76003196d3d351425116c
thaidq2 <thaidq2@topica.edu.vn>
2018-10-15 19:44:29 +0700

    add navigate function

--- src/app/test-center/test-select/test-select.component.ts
+++ src/app/test-center/test-select/test-select.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
-  selector: 'app-test-select',
-  templateUrl: './test-select.component.html',
-  styleUrls: ['./test-select.component.css']
+    selector: 'app-test-select',
+    templateUrl: './test-select.component.html',
+    styleUrls: ['./test-select.component.css'],
+    animations: [
+        trigger("openClose", [
+            state("open", style({
+                height: '160px'
+            })),
+            state("closed", style({
+                height: '0px'
+            })),
+            transition("open => closed", [
+                animate('0.2s'),
+            ]),
+            transition("closed => open", [
+                animate('0.1s'),
+            ])
+        ])
+    ]
 })
 export class TestSelectComponent implements OnInit {
+    @Input() questionCodes: any[];
+    @Input() order: number;
+    isShowOption: boolean;
+    constructor() {
+        this.isShowOption = false;
+    }
 
-  constructor() { }
+    ngOnInit() {
+        console.log(this.questionCodes);
+    }
 
-  ngOnInit() {
-  }
+    handleSelect(): void {
 
+    }
+
+    showOption(): void {
+        this.isShowOption = !this.isShowOption;
+    }
 }
